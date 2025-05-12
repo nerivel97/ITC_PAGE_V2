c718f696833ef599df2cd6c527bb18ca4f473ab9
joseeduardohg <101235315+joseeduardohg@users.noreply.github.com>
2025-05-12 17:40:59 -0500

    feat(api): add repositories helpers for typeorm repositories

---
+++ backend/src/repositories/oferta-educativa/reticula.repository.ts
@@ -0,0 +1,4 @@
+import { AppDataSource } from '../../config/database';
+import { Reticle } from '../../models/oferta-educativa/Reticula';
+
+export const ReticleRepository = AppDataSource.getRepository(Reticle);
