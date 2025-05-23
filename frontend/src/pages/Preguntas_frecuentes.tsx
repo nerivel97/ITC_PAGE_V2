6ae93f63aba185f89b60d2498e0e930f3922e31d
Jusit-Toledo <149429020+Jusit-Toledo@users.noreply.github.com>
2025-05-23 00:19:56 -0500

    Agrega pagina de preguntas frecuentes

---
+++ frontend/src/pages/Preguntas_frecuentes.tsx
@@ -0,0 +1,11 @@
+import Barrapreguntas from "../components/PreguntasFrecuentes/barrapreguntas";
+
+const Preguntas_frecuentes = () => {
+  return (
+    <div>
+      <Barrapreguntas />
+    </div>
+  );
+};
+
+export default Preguntas_frecuentes;
