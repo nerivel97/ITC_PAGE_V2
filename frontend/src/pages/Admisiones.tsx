de5afc255b4710460dcdd033158703082abdfd67
Trashah <l21530323@cancun.tecnm.mx>
2025-06-02 17:32:43 -0500

    Arreglo en imagenes de pagina de admisiones

--- frontend/src/pages/Admisiones.tsx
+++ frontend/src/pages/Admisiones.tsx
@@ -2,6 +2,10 @@ import React from 'react';
 import Card from '../components/AdmisionesComponents/ComponentCard/Card';
 import Title from '../components/AdmisionesComponents/ComponentTitle/Title';
 import styles from '../components/AdmisionesComponents/Home.module.css';
+import libroImage from '../assets/Fotos/Admisiones/Libro.png';
+import escudoImage from '../assets/Fotos/Admisiones/Escudo.png';
+import redImage from '../assets/Fotos/Admisiones/Red.png';
+
 
 const Admisiones: React.FC = () => {
     return (
@@ -11,34 +15,44 @@ const Admisiones: React.FC = () => {
                 <Title> Oferta Educativa TECNM Campus Cancún 2025 </Title>
             </div>
 
-            <Card imageUrl="../../assets/Fotos/Admisiones/Libro.png" 
-            title='GUÍA PARA EL EXAMEN DE SELECCIÓN'
-            description='Presenta la oferta educativa a distancia en sedes como Puerto Morelos y Chunhuhub. Contiene requisitos, fechas, examen en línea y detalles para solicitar beca.'
-            buttonText = 'Descargar PDF'
-            pdfUrl = '/Documentos/Guia-Examen-Seleccion-2025.pdf'
-            reverse={true} />
+            <Card
+                imageUrl={libroImage}
+                title="GUÍA PARA EL EXAMEN DE SELECCIÓN"
+                description="Presenta la oferta educativa a distancia en sedes como Puerto Morelos y Chunhuhub. Contiene requisitos, fechas, examen en línea y detalles para solicitar beca."
+                buttonText="Descargar PDF"
+                pdfUrl="/Documentos/Guia-Examen-Seleccion-2025.pdf"
+                reverse={true}
+            />
 
-            <Card imageUrl="../../assets/Fotos/Admisiones/Escudo.png" 
-            title='2DA CONVOCATORIA CAMPUS CANCÚN 2025'
-            description='Incluye requisitos, fechas, carreras disponibles y pasos para ingresar en modalidad presencial al semestre agosto - diciembre 2025. También detalla pagos e inscripción.'
-            buttonText = 'Descargar PDF'
-            pdfUrl = '/Documentos/2da-Convocatoria-Campus-Cancun-2025.pdf'
-            reverse={false} />
+            <Card
+                imageUrl={escudoImage}
+                title="2DA CONVOCATORIA CAMPUS CANCÚN 2025"
+                description="Incluye requisitos, fechas, carreras disponibles y pasos para ingresar en modalidad presencial al semestre agosto - diciembre 2025. También detalla pagos e inscripción."
+                buttonText="Descargar PDF"
+                pdfUrl="/Documentos/2da-Convocatoria-Campus-Cancun-2025.pdf"
+                reverse={false}
+            />
 
-            <Card imageUrl="../../assets/Fotos/Admisiones/Red.png" 
-            title='CONVOCATORIA EDUCACIÓN A DISTANCIA 2025'
-            description='Presenta la oferta educativa a distancia en sedes como Puerto Morelos y Chunhuhub. Contiene requisitos, fechas, examen en línea y detalles para solicitar beca.'
-            buttonText = 'Descargar PDF'
-            pdfUrl = '/Documentos/Convocatoria-Educacion-A-Distancia.pdf'
-            reverse={true} />
+            <Card
+                imageUrl={redImage}
+                title="CONVOCATORIA EDUCACIÓN A DISTANCIA 2025"
+                description="Presenta la oferta educativa a distancia en sedes como Puerto Morelos y Chunhuhub. Contiene requisitos, fechas, examen en línea y detalles para solicitar beca."
+                buttonText="Descargar PDF"
+                pdfUrl="/Documentos/Convocatoria-Educacion-A-Distancia.pdf"
+                reverse={true}
+            />
 
-            <Card imageUrl="../../assets/Fotos/Admisiones/Libro.png" 
-            title='RESULTADOS'
-            description='Revisa los resultados de los exámenes de admisiones.'
-            buttonText='Ver más'
-            reverse={false}
-            linkUrl = '/Resultados' />
+            <Card
+                imageUrl={libroImage}
+                title="RESULTADOS"
+                description="Revisa los resultados de los exámenes de admisiones."
+                buttonText="Ver más"
+                linkUrl="/Resultados"
+                reverse={false}
+            />
+
         </div>
     );
 };
+
 export default Admisiones;
